edce60934dfa947f1eebe558a141cff313855e6a
공대표 <kongrpt@gmail.com>
2021-08-04 13:29:29 +0900

    로그인까지 구현 완료

--- src/main/java/springboot/sm/controller/MemberController.java
+++ src/main/java/springboot/sm/controller/MemberController.java
@@ -1,18 +1,25 @@
 package springboot.sm.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import springboot.sm.domain.loginform.LoginForm;
 import springboot.sm.domain.Member;
 import springboot.sm.service.MemberService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     @Autowired
@@ -24,25 +31,39 @@ public class MemberController {
     }
 
     @GetMapping("/login")
-    public String loginForm(@ModelAttribute Member member) {
+    public String loginForm(@ModelAttribute LoginForm loginForm) {
 
-        System.out.println("LoginForm");
-        return "members/LoginForm";
+
+        return "members/loginForm";
     }
 
-
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginform, BindingResult bindingResult,
+                      @RequestParam(defaultValue = "/") String redirectUrl,HttpServletRequest httpServletRequest){
+        if (bindingResult.hasErrors()) return "members/loginForm";
+        Member loginMember = memberService.login(loginform.getLoginId(), loginform.getPassword());
+        log.info("loginMember={}",loginMember);
+        if (loginMember == null){
+            bindingResult.reject("loginFail","loginId OR password ERROR");
+            return "members/loginForm";
+        }
+//        HttpSession session = httpServletRequest.getSession();
+//        session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
+//        session.setAttribute(SessionConst.WRITER,form.getLoginId());
+        return "redirect:"+redirectUrl;
+    }
+    /** DP_ DB 필드와 변수이름 맞추는게 안전 (오류난적이씀 ㅠㅠ) **/
 
     @GetMapping("/signUp")
     public String signUpForm(@ModelAttribute Member member) {
 
-
-        return "members/SignUpForm";
+        return "members/signUpForm";
     }
 
     @PostMapping("/signUp")
     public String memberSave(@ModelAttribute Member member, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "members/addMemberForm";
+            return "members/signUpForm";
         }
         /** DP_ Member객체인 member로 signUpForm 이동 **/
         memberService.memberSave(member);
