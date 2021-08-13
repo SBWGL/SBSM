411a339b5c602c5b61741307aefecc677ea5d2e0
lehehwan <rdsa257@naver.com>
2021-08-13 20:04:18 +0900

    장바구니 미완

--- src/main/java/springboot/sm/controller/BasketController.java
+++ src/main/java/springboot/sm/controller/BasketController.java
@@ -1,2 +1,19 @@
-package springboot.sm.controller;public class BasketController {
+package springboot.sm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BasketController {
+
+    @GetMapping("/basket")
+    public String basketPage(){
+        return "products/basket";
+    }
+
+    @PostMapping("/basket")
+    public String basketPage1(){
+        return "products/basket";
+    }
 }
--- src/main/java/springboot/sm/domain/Basket.java
+++ src/main/java/springboot/sm/domain/Basket.java
@@ -1,2 +1,17 @@
-package springboot.sm.domain;public class Basket {
+package springboot.sm.domain;
+
+import lombok.Data;
+
+@Data
+public class Basket {
+
+    private int cartId;
+    private String loginId;
+    private int productId;
+    private String storeImageName;
+    private String color;
+    private String size;
+    private int quantity;
+    private int price;
+    private int totalPrice;
 }
